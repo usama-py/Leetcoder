9d74562ab884a5a1a451841c2e0a38abd0c68047
usama-py <104300164+usama-py@users.noreply.github.com>
2022-10-28 23:17:38 +0530

    Time: 403 ms (19.52%), Space: 15.2 MB (43.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,3 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         return sum(list([i for i,x in enumerate(nums, 1)]))-sum(nums)
-        # a = [-1]*(max(nums)+1)
-        # for i in range(len(nums)):    
-        #     a[nums[i]] = nums[i]
-        # for i in range(len(a)):
-        #     if a[i] == -1:
-        #         return i
-        # return len(a)
