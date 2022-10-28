b8d7100fa5f7974c7c7f8a236aff21ed0eb65478
usama-py <104300164+usama-py@users.noreply.github.com>
2022-10-28 22:51:24 +0530

    Time: 781 ms (24.94%), Space: 27.3 MB (57.79%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,19 +2,18 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         if min(nums) > 1:
             return 1
-        if max(nums) < 0:
+        maxi = max(nums)
+        if maxi < 0:
             return 1
-        if max(nums) > 100001:
+        if maxi > 100001:
             a = [-1]*100001
         else:
-            a = [-1]*(max(nums)+1)
+            a = [-1]*(maxi+1)
         a[0] = 0
         for i in range(len(nums)):
             if nums[i] > 0 and nums[i] < 100001:
                 a[nums[i]] = nums[i]
         for i in range(len(a)):
             if a[i] == -1:
-                # print("--- %s seconds ---" % (time.time() - start_time))
                 return i
-        # print("--- %s seconds ---" % (time.time() - start_time))
         return len(a)
