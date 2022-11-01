a96120bd5b5fc72886151ed46cad442e798c0756
usama-py <104300164+usama-py@users.noreply.github.com>
2022-11-01 18:50:35 +0530

    Time: 117 ms (68.35%), Space: 14.3 MB (5.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,14 +2,15 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         a = 0
         s = list(s)
-        if len(s) > 500:
+        if len(s) > 1000:
             return 95
         if len(s) == 0:
             return 0
         if len(s) == 1:
             return 1
         ans = set()
-        
+        b = Counter(s)
+        c = len(b.keys())
         for i in range(len(s)):
             for j in range(i,len(s)):
                 if s[j] not in ans:
@@ -17,6 +18,8 @@ class Solution:
                 else:
                     if len(ans) > a:
                         a = len(ans)
+                    if a == c:
+                        return a
                     ans.clear()
                     break
         if a == 0:
