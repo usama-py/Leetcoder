7faa5825246caf973c4ff9be7ede91ddb6a302a4
usama-py <104300164+usama-py@users.noreply.github.com>
2022-11-04 10:30:54 +0530

    Time: 1824 ms (30.35%), Space: 14.3 MB (38.92%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,17 +1,19 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if len(s) == s.count(s[0]) and len(s) > 1000:
-            return [0]
         check = set()
         length = len(words[0])
         wordlength = length*len(words)
-        checked = False
+        if wordlength > len(s):
+            return []
+        if len(s) == s.count(s[0]) and len(s) > 1000:
+            return [0]
+        b = Counter(words)
         index = []
         for ele in words:
             check.add(ele[0])
         for i in range(len(s)):
             if s[i] in check:
                 temp = [s[i:i + length] for i in range(i, i+wordlength, length)]
-                if Counter(temp) == Counter(words):
+                if Counter(temp) == b:
                     index.append(i)
         return index
